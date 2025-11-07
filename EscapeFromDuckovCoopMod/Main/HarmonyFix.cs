2331993922e330369123ad3da100d1e997e02485
Neko17awa <neko17lt@163.com>
2025-11-08 05:14:25 +0800

    合入#93 修复暂停菜单仍然能开火的问题

--- EscapeFromDuckovCoopMod/Main/HarmonyFix.cs
+++ EscapeFromDuckovCoopMod/Main/HarmonyFix.cs
@@ -1,4 +1,4 @@
-// Escape-From-Duckov-Coop-Mod-Preview
+﻿// Escape-From-Duckov-Coop-Mod-Preview
 // Copyright (C) 2025  Mr.sans and InitLoader's team
 //
 // This program is not a free software.
@@ -299,7 +299,7 @@ internal static class Patch_Paused_AlwaysFalse
         var mod = ModBehaviourF.Instance;
         if (mod == null || !mod.networkStarted) return true;
 
-        __result = false;
+        __result = mod.Pausebool;
 
         return false;
     }
